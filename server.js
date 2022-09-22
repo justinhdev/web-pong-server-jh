4b8e0aa8df9ef17be5a13f5cf46b4ac737a1bd96
jhancockdev <jhancockpta@gmail.com>
2022-09-22 10:58:13 -0500

    commit cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://127.0.0.1:5501"
+    origin: "https://web-2pong.netlify.app/"
   }
 });
 const PORT = process.env.PORT || 3000;
