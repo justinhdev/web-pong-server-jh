d66eed4b893e507d10eecae460aebce231c9a5cd
jhancockdev <jhancockpta@gmail.com>
2022-09-24 09:20:03 -0500

    testing

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
     
-    origin: "https://web-2pong.netlify.app"
+    origin: "http://127.0.0.1:5501/client/index.html"
   }
 });
 const PORT = process.env.PORT || 3000;
